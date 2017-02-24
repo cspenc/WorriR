3e21876a6ad68ca06c3c85a69252d7e3ad3de5b8
cspenc <crs992@gmail.com>
2017-02-24 13:59:38 -0500

    change file structure a little

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import '../../public/styling/App.css';
+import '../public/styling/App.css';
 
-import SearchBar from './search_bar';
+import SearchBar from './components/search_bar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SearchBar />
-        Yo weenis time, I'm a weenie.
+        This is the homepage.
       </div>
     );
   }
