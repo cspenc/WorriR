221f6cd941f209de2393d7e8628889266b287d3c
cspenc <crs992@gmail.com>
2017-02-26 18:57:14 -0500

    removed lodash

--- front-end/src/components/worry_list.js
+++ front-end/src/components/worry_list.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 
 import WorryItem from './worry_item';
 
 class WorryList extends Component {
 
-  renderWorries() {
-    return _.map(this.props.worry, (worry, index) => <WorryItem key={index} {...worry} />);
-  }
-
   render() {
+    let worries = this.props.worry.map((worry, index) => {
+      return <WorryItem key={index} {...worry} />
+    })
+    
     return (
       <div>
         <h1>WorryList</h1>
         <ul>
-          {this.renderWorries()}
+          {this.worries}
         </ul>
       </div>
     )
