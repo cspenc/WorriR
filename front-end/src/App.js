e07d94a0a0937a72396549c2d98f0a3ad86cf6ad
cspenc <crs992@gmail.com>
2017-02-26 14:06:50 -0500

    back to the beginning

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import '../public/styling/App.css';
 
-import SearchBar from './components/search_bar';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar />
         This is the homepage.
       </div>
     );
