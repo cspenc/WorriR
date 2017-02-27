bfa2efbb3fe99d6445ac022bd8fec3e20565f325
cspenc <crs992@gmail.com>
2017-02-27 14:44:20 -0500

    break

--- front-end/src/components/create_worry.js
+++ front-end/src/components/create_worry.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class CreateWorry extends Component {
+
+  handleCreate(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleCreate.bind(this)}>
           <input type="text" placeholder="worry" />
           <button>Add</button>
         </form>
