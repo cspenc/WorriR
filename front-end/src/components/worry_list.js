60a1ec5f515903cd5829547f47bf8910b52e89aa
cspenc <crs992@gmail.com>
2017-02-26 21:23:59 -0500

    adding edit features

--- front-end/src/components/worry_list.js
+++ front-end/src/components/worry_list.js
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 import WorryItem from './worry_item';
 
 class WorryList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isEditing: false
+    };
+  }
+
+  onEditClick(event) {
+    this.setState({ isEditing: true });
+  }
 
   render() {
     let worries = this.props.worry.map((worry, index) => {
       return <WorryItem key={index} {...worry} />
     })
-    
+
     return (
       <div>
         <h1>WorryList</h1>
